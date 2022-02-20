442ed825e55cb8783375c1f6f777729f2aff08fe
Luis Rujana <lmrujanass@gmail.com>
2022-02-20 12:49:39 -0500

    Replaced custom async middleware for express-async-error

--- routes/customers.js
+++ routes/customers.js
@@ -2,101 +2,80 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { Customer, validate } = require("../models/customer");
 const auth = require("../middleware/auth");
-const asyncMiddleware = require("../middleware/async");
 
 const router = express.Router();
 
 //GET
-router.get(
-  "/",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const customers = await Customer.find();
-    res.send(customers);
-  })
-);
+router.get("/", auth, async (req, res) => {
+  const customers = await Customer.find();
+  res.send(customers);
+});
 
-router.get(
-  "/:id",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    let customer;
-    try {
-      customer = await Customer.findById(req.params.id);
-    } catch (ex) {
-      console.log(ex.message);
-    }
+router.get("/:id", auth, async (req, res) => {
+  let customer;
+  try {
+    customer = await Customer.findById(req.params.id);
+  } catch (ex) {
+    console.log(ex.message);
+  }
 
-    if (!customer) {
-      res.status(404).send(`No customer with id ${req.params.id} found...`);
-    }
+  if (!customer) {
+    res.status(404).send(`No customer with id ${req.params.id} found...`);
+  }
 
-    res.send(customer);
-  })
-);
+  res.send(customer);
+});
 
 //POST
-router.post(
-  "/",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.message);
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.message);
 
-    const newCustomer = new Customer({
+  const newCustomer = new Customer({
+    name: req.body.name,
+    phone: req.body.phone,
+    isGold: req.body.isGold,
+  });
+
+  try {
+    await newCustomer.save();
+    console.log(newCustomer);
+    res.send(newCustomer);
+  } catch (ex) {
+    console.log(ex.message);
+    return res.status(400).send(`Failed to saved new customer.`);
+  }
+});
+
+//PUT
+router.post("/:id", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.message);
+
+  const customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    {
       name: req.body.name,
       phone: req.body.phone,
       isGold: req.body.isGold,
-    });
+    },
+    { new: true }
+  );
 
-    try {
-      await newCustomer.save();
-      console.log(newCustomer);
-      res.send(newCustomer);
-    } catch (ex) {
-      console.log(ex.message);
-      return res.status(400).send(`Failed to saved new customer.`);
-    }
-  })
-);
+  if (!customer)
+    res.status(404).send(`No customer with id ${req.body.id} found...`);
 
-//PUT
-router.post(
-  "/:id",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.message);
-
-    const customer = await Customer.findByIdAndUpdate(
-      req.params.id,
-      {
-        name: req.body.name,
-        phone: req.body.phone,
-        isGold: req.body.isGold,
-      },
-      { new: true }
-    );
-
-    if (!customer)
-      res.status(404).send(`No customer with id ${req.body.id} found...`);
-
-    res.send(customer);
-  })
-);
+  res.send(customer);
+});
 
 //DELETE
-router.delete(
-  "/:id",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const customer = await Customer.findByIdAndDelete(req.params.id);
+router.delete("/:id", auth, async (req, res) => {
+  const customer = await Customer.findByIdAndDelete(req.params.id);
 
-    if (!customer)
-      res.status(404).send(`No customer with id ${req.body.id} found...`);
+  if (!customer)
+    res.status(404).send(`No customer with id ${req.body.id} found...`);
 
-    res.send(customer);
-  })
-);
+  res.send(customer);
+});
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -4,28 +4,20 @@ const express = require("express");
 const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
 const auth = require("../middleware/auth");
-const asyncMiddleware = require("../middleware/async");
 
 const router = express.Router();
 
 //GET
-router.get(
-  "/",
-  asyncMiddleware(async (req, res) => {
-    const users = await User.find().sort("name");
-    res.send(users);
-  })
-);
+router.get("/", async (req, res) => {
+  const users = await User.find().sort("name");
+  res.send(users);
+});
 
-router.get(
-  "/me",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    console.log(req.user);
-    const user = await User.findById(req.user._id).select("-password");
-    res.send(user);
-  })
-);
+router.get("/me", auth, async (req, res) => {
+  console.log(req.user);
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 
 // router.get("/:id", async (req, res) => {
 //   const user = await User.findById(req.params.id);
@@ -36,32 +28,26 @@ router.get(
 // });
 
 //POST
-router.post(
-  "/",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const { error } = validate(req.body);
-    if (error)
-      return res.status(400).send(`Invalid user. ${error.details[0].message}`);
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error)
+    return res.status(400).send(`Invalid user. ${error.details[0].message}`);
 
-    let user = await User.findOne({ email: req.body.email });
-    if (user) return res.status(400).send("Email already registered");
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("Email already registered");
 
-    const newUser = new User(_.pick(req.body, ["name", "email", "password"]));
-    const salt = await bcrypt.genSalt(10);
-    newUser.password = await bcrypt.hash(newUser.password, salt);
+  const newUser = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  newUser.password = await bcrypt.hash(newUser.password, salt);
 
-    try {
-      await newUser.save();
-      console.log(newUser);
-      const token = newUser.generateAuthToken();
-      res
-        .header("x-auth-token", token)
-        .send(_.pick(newUser, ["name", "email"]));
-    } catch (ex) {
-      console.log(ex);
-    }
-  })
-);
+  try {
+    await newUser.save();
+    console.log(newUser);
+    const token = newUser.generateAuthToken();
+    res.header("x-auth-token", token).send(_.pick(newUser, ["name", "email"]));
+  } catch (ex) {
+    console.log(ex);
+  }
+});
 
 module.exports = router;
